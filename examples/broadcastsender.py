3e755152a176e73bb742cd097a06705e6f332907
iowi479 <simon@alle-schindlers.de>
2024-12-18 11:52:46 +0100

    first steps on startup

--- examples/broadcastsender.py
+++ examples/broadcastsender.py
@@ -1,11 +1,6 @@
 import socket
 
 
-def broadcast(ip, port, broadcast_message):
-    broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    broadcast_socket.sendto(str.encode(broadcast_message), (ip, port))
-    broadcast_socket.close()
 
 
 if __name__ == '__main__':
@@ -16,5 +11,33 @@ if __name__ == '__main__':
     MY_IP = socket.gethostbyname(MY_HOST)
 
     message = MY_IP + ' sent a broadcast'
-    broadcast(BROADCAST_IP, BROADCAST_PORT, message)
+
+    broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    broadcast_socket.sendto(str.encode(message), (BROADCAST_IP, BROADCAST_PORT))
+
+    while True:
+        data, addr = broadcast_socket.recvfrom(1024)
+        if data:
+            print("Received broadcast message:", data.decode())
+            break
+
+        # check for timeout here, if there is no previous ring
+        if not data:
+            print("No previous ring found. Creating a new one.")
+            break
+
+    broadcast_socket.close()
+
+
+    # 1. Send a broadcast message to all participants in the network
+    # 2. Listen to a response from a participant
+    #   2.1. If a response with the current members is received, add yourself to the list of members
+    #   2.2. If a response with the current members is not received, there is no current ring available. So create a new one.
+
+    # 3. Start a new election in the new ring.
+    #   3.1. If the ring is only us, we are the leader
+    #   3.2. If there was a previous ring, the election determines the leader
+
+    # 4. Wait for the election to finish
 
